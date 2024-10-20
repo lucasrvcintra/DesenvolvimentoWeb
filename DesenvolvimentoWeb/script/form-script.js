89d6a0ffcc3777f8bd572c9b5b01f6e8c26bd685
Lucas Cintra <lucasrcintra63@gmail.com>
2024-10-20 20:38:54 -0300

    capturando dados do form

---
+++ DesenvolvimentoWeb/script/form-script.js
@@ -0,0 +1,18 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.contact form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value,
+    };
+
+    console.log('Dados do Formulário:', formData);
+
+    form.reset();
+  });
+});
